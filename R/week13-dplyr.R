d0bfa3161a3b053d9b29f8b03f8d7a7c0eaa1b19
[chulinchen20] <[chen6496@umn.edu]>
2024-04-28 16:29:21 -0500

    update week13-dplyr.R

--- R/week13-dplyr.R
+++ R/week13-dplyr.R
@@ -18,20 +18,32 @@ databases_df <- dbGetQuery(conn, "SHOW DATABASES;")
 dbExecute(conn, "USE cla_tntlab;")
 dbGetQuery(conn, "SHOW TABLES")
 
-datascience_8960_table = dbGetQuery(conn, 
-                                    "SELECT * FROM datascience_8960_table")
-write_csv(datascience_8960_table, "../data/week13.csv")
+employees_tbl = dbGetQuery(conn, 
+                                    "SELECT * FROM datascience_employees") 
+write_csv(employees_tbl, "../data/employees.csv")
 
-# Open saved dataset 
-week13_tbl = read_csv("../data/week13.csv", show_col_types = FALSE)
+testscores_tbl = dbGetQuery(conn, 
+                           "SELECT * FROM datascience_testscores") 
+write_csv(testscores_tbl, "../data/testscores.csv")
+
+offices_tbl = dbGetQuery(conn, 
+                           "SELECT * FROM datascience_offices") 
+write_csv(offices_tbl, "../data/offices.csv")
+
+# combine dataset an remove employees without test scores 
+week13_tbl = inner_join(employees_tbl,testscores_tbl,by="employee_id")
+write_csv(week13_tbl, "../data/week13.csv")
 
 ##### Analysis #####
 
 # Display the total number of managers.
-nrow(week13_tbl)
+week13_tbl %>%
+  summarize(total_rows = n())
 
 # Display the total number of unique managers.
-length(unique(week13_tbl$employee_id))
+week13_tbl %>%
+  distinct(employee_id) %>%  
+  summarize(total_unique_rows = n())
 
 # Display a summary of the number of managers split by location, 
 #but only include those who were not originally hired as managers.
@@ -46,12 +58,11 @@ week13_tbl %>%
   group_by(performance_group) %>%
   summarise(avg_yrs = mean(yrs_employed), sd_yrs = sd(yrs_employed))
 
-# Display the location and ID numbers of the top 3 managers from each location, 
+# Display the location and ID numbers of each manager from each location, 
 # in alphabetical order by location and then descending order of test score. 
-# If there are ties, include everyone reaching rank 3. 
+
 week13_tbl %>% 
   select(city,employee_id, test_score) %>%
   group_by(city) %>%
   mutate(ranking = dense_rank(desc(test_score))) %>%
-  filter(ranking<=3) %>%
   arrange(city, desc(test_score))
